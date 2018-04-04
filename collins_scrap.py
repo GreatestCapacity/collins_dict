e5656fcf94a200f8aa667fd8bc0efb5e8a0bc313
GreatestCapacity <38064441+GreatestCapacity@users.noreply.github.com>
2018-04-04 14:58:50 +0800

    Add files via upload

---
+++ collins_scrap.py
@@ -0,0 +1,18 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import re
+
+query_url = 'http://www.iciba.com/'
+
+
+# str -> list[str]
+def query(word):
+    word.replace(' ', '%20')
+    html = urlopen(query_url + word)
+    bsObj = BeautifulSoup(html.read(), 'html.parser')
+    content = bsObj.select('div.collins-section')[0]
+    lst = []
+    for part in content.children:
+        content = re.sub(r'<.*?>', '', str(part))
+        lst += list(filter(lambda x: len(x) != 0, content.split('\n')))
+    return lst
